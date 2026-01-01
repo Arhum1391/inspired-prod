f91891d19c8fac77f5b95b4f52e9251a77844512
ayesha <aishaehsan0@gmial.com>
2026-01-01 14:48:06 +0500

    price update

--- src/components/pages/MeetingsPage.tsx
+++ src/components/pages/MeetingsPage.tsx
@@ -255,6 +255,10 @@ const ANALYST_CUSTOM_PRICING: Record<string, DurationPricing> = {
     30: 300,
     60: 400,
   },
+  meower:{
+    30: 1,
+    60: 1,
+  }
 };
 
 const DEFAULT_PRICING: DurationPricing = {
